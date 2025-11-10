c64a42a0f91fdc114310e7c2d39c98e7e0c14307
Gastón <162315968+chispamaya@users.noreply.github.com>
2025-11-10 16:17:54 -0300

    Falta un poco

--- backend/mediosAudioVisuales-APIs/src/main/java/com/example/demo/service/UsuarioService.java
+++ backend/mediosAudioVisuales-APIs/src/main/java/com/example/demo/service/UsuarioService.java
@@ -1,5 +1,80 @@
 package com.example.demo.service;
+import com.example.demo.dto.PerfilDTO;
+import com.example.demo.dto.Rol;
+import com.example.demo.dto.Usuario;
 
+import com.example.demo.repository.UsuarioRepository;
+import com.example.demo.repository.RolRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UsuarioService {
 
-}
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private RolRepository rolRepository;
+    
+    public List<Usuario> listarUsuarios() {
+        
+        return usuarioRepository.listarTodosLosUsuarios();
+    }
+
+ 
+    public Usuario buscarUsuarioPorId(Long id) {
+        return usuarioRepository.buscarUsuarioPorId(id);
+    }
+
+
+    public String crearUsuario(Usuario nuevoUsuario, Long idUsuarioQueCrea) {
+    	
+    	Usuario usuarioExistente = usuarioRepository.buscarUsuarioPorEmail(nuevoUsuario.getEmail());
+    	
+    	
+    	if (usuarioExistente != null) {
+            return "Error: El email '" + nuevoUsuario.getEmail() + "' ya está registrado.";
+        }        
+    	if (nuevoUsuario.getContrasenia().length() < 8) {
+            return "Error: La contraseña debe tener al menos 8 caracteres.";
+        }
+    	
+    	return usuarioRepository.crearUsuario(nuevoUsuario, idUsuarioQueCrea);
+    }
+
+  
+    public String borrarUsuario(Long idUsuarioABorrar, Long idUsuarioQueBorra) {
+        
+        return usuarioRepository.borrarUsuario(idUsuarioABorrar, idUsuarioQueBorra);
+    }
+    
+    public String modificarRolUsuario(Long idUsuarioAModificar, Long idNuevoRol, Long idUsuarioQueModifica) {
+        return usuarioRepository.modificarRolDeUsuario(idUsuarioAModificar, idNuevoRol, idUsuarioQueModifica);
+    }
+    
+    
+	public PerfilDTO obtenerDatosPerfil(Long idUsuario) {
+	        
+	        Usuario usuario = usuarioRepository.buscarUsuarioPorId(idUsuario);
+	
+	        if (usuario == null) {
+	            return null; 
+	        }
+
+	        Rol rol = rolRepository.buscarRolPorId(usuario.getIdRol());
+	
+	        if (rol == null) {
+	            return null; 
+	        }
+	
+	        PerfilDTO perfil = new PerfilDTO();
+	        perfil.setNombreUsuario(usuario.getNombre());
+	        perfil.setNombreRol(rol.getNombre());     
+	
+	        return perfil;
+	    }
+	}
