236935d36c421f02392d9e60b1c7ff681299e8f2
Krauchii <162316114+Krauchii@users.noreply.github.com>
2025-11-17 00:57:56 -0300

    Modificado para conexión react y httpcats

--- backend/mediosAudioVisuales-APIs/src/main/java/com/example/demo/MediosAudioVisualesApIsApplication.java
+++ backend/mediosAudioVisuales-APIs/src/main/java/com/example/demo/MediosAudioVisualesApIsApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MediosAudioVisualesApIsApplication {
@@ -9,5 +12,16 @@ public class MediosAudioVisualesApIsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MediosAudioVisualesApIsApplication.class, args);
 	}
-
+	// --- AGREGAR ESTO PARA ARREGLAR EL ERROR DE CONEXIÓN ---
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**") // Permite todas las rutas de la API
+							.allowedOrigins("http://localhost:5173") // Permite SOLO a tu frontend (chequeá que este sea tu puerto)
+							.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // Permite estos métodos
+				}
+			};
+}
 }
