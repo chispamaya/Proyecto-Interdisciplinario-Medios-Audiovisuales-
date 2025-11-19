8a99be03df9a7f537fcf60dc9354ffd8f967a711
Krauchii <162316114+Krauchii@users.noreply.github.com>
2025-11-19 03:02:13 -0300

    Actualizado

--- backend/mediosAudioVisuales-APIs/src/main/java/com/example/demo/dto/EncuestaResultado.java
+++ backend/mediosAudioVisuales-APIs/src/main/java/com/example/demo/dto/EncuestaResultado.java
@@ -1,27 +1,18 @@
 package com.example.demo.dto;
 
 import java.util.Date;
-
 import lombok.Data;
 
-/**
- * DTO (Molde) que representa UNA FILA del resultado
- * que devuelve tu SP s('encuesta', id, @m).
- * Es un "molde a medida" para el JOIN de 3 tablas que hace tu SP.
- */
 @Data
 public class EncuestaResultado {
-
-    // Info de la Encuesta (e)
     private Long idEncuesta;
     private String preguntar;
     private Long idCreador;
-
-    // Info de la Opción (o)
     private Long idOpcion;
     private String opcion;
-
-    // Info de los Votos (v)
     private Long totalVotos;
     private Date fechaCreacion;
+
+    // NUEVO: Si el usuario votó esta opción específica
+    private boolean votadaPorMi; 
 }
