063b3cdb581d123825e6573461003be7a51daf9e
Krauchii <162316114+Krauchii@users.noreply.github.com>
2025-11-19 03:00:57 -0300

    Actualizado

--- backend/mediosAudioVisuales-APIs/src/main/java/com/example/demo/dto/Contenido.java
+++ backend/mediosAudioVisuales-APIs/src/main/java/com/example/demo/dto/Contenido.java
@@ -1,16 +1,19 @@
 package com.example.demo.dto;
 
 import java.util.Date;
-
+import java.util.List;
 import lombok.Data;
 
 @Data
 public class Contenido {
-
     private Long id;
     private String formato;
     private String rutaArchivo;
-    private String texto; // Campo que SÍ existe en tu tabla 'contenidos'
+    private String texto;
     private Long idUsuario;
     private Date fechaCreacion;
+    private List<Long> tags;
+    
+    // NUEVO: Estado del usuario actual
+    private Boolean miReaccion; // true=Like, false=Dislike, null=Nada
 }
