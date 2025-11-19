b574352856742dfdae5d31774613b515a8f36d2d
Krauchii <162316114+Krauchii@users.noreply.github.com>
2025-11-18 23:53:23 -0300

    Add files via upload

---
+++ backend/mediosAudioVisuales-APIs/src/main/java/com/example/demo/controller/FeedController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.PublicacionDTO;
+import com.example.demo.service.FeedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/feed")
+@CrossOrigin(origins = "http://localhost:5173")
+public class FeedController {
+
+    @Autowired
+    private FeedService feedService;
+
+    @GetMapping
+    public List<PublicacionDTO> obtenerFeed() {
+        return feedService.obtenerFeedUnificado();
+    }
+}
